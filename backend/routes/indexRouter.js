1233757667af3e395227548d8c97629b1ba35305
Bhupinder <bhupindermalhotra64@gmail.com>
2024-09-09 17:15:25 +0530

    implemented pagination

--- backend/routes/indexRouter.js
+++ backend/routes/indexRouter.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
-const productModel = require("../models/product-model")
+const productModel = require("../models/product-model");
 
-router.get("/shop",async function(req, res) {
-    try {
-        let products =  await productModel.find();
-        // console.log(products.image)
-        res.json({products})
-    } catch (error) {
-        res.json({errorInfetching: true})
-    }
-   
-})
+router.get("/shop", async function (req, res) {
+  // try {
+  //     let products =  await productModel.find();
+  //     // console.log(products.image)
+  //     res.json({products})
+  // } catch (error) {
+  //     res.json({errorInfetching: true})
+  // }
 
-module.exports = router
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    let startIndex = (page - 1) * limit;
+
+    // let indexing = await productModel.createIndexes({ _id: 1 });
+
+    // let products = await productModel.find();
+    let products = await productModel
+    .find()
+    .sort({ _id: 1 })  // Sorting by indexed _id
+    .skip(startIndex)
+    .limit(limit);
+    //  res.json({running: true})
+    const totalProducts = await productModel.countDocuments();
+    totalPages = Math.ceil(totalProducts / limit);
+
+    // res.json({ products, totalPages, page, limit });
+    res.json({ products, totalPages });
+  } catch (error) {
+    res.json({ errorInfetching: true });
+  }
+});
+
+module.exports = router;
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,23 +4,38 @@ import axios from "axios";
 
 const Home = () => {
   const [productData, setProductData] = useState();
+  const [totalPages, setTotalPages] = useState();
+  const [page, setPage] = useState(1);
+  const [activePage, setActivePage] = useState(null);
+
+  const handlepageclick = (index) => {
+    setPage(index + 1);
+    setActivePage(index + 1);
+  };
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/shop");
+      const response = await axios.get(
+        `http://localhost:5000/shop?page=${page}&limit=6`
+      );
       setProductData(response.data);
-      // setLoading(false);
-    } catch (err) {
-      // setError(err);
-      // setLoading(false);
-    }
+      let totalpages = response?.data?.totalPages;
+
+      let pages = [];
+      for (let i = 1; i <= totalpages; i++) {
+        pages.push(i);
+      }
+      setTotalPages(pages);
+    } catch (err) {}
   };
+
+  // console.log(totalPages);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [page]);
   let allproducts = productData?.products;
-  console.log(allproducts);
+  // console.log(allproducts);
 
   return (
     <div className=" w-full fixed top-0 left-0">
@@ -69,13 +84,20 @@ const Home = () => {
           </div>
           <div className="w-[75%] flex flex-row flex-wrap gap-10 max-h-[500px] scrollbar-hide overflow-y-auto">
             {allproducts?.map((product) => {
-               const base64String = product?.image?.data?.data 
-               ? btoa(String.fromCharCode(...new Uint8Array(product.image.data.data)))
-               : '';
-              return  (
+              const base64String = product?.image?.data?.data
+                ? btoa(
+                    String.fromCharCode(
+                      ...new Uint8Array(product.image.data.data)
+                    )
+                  )
+                : "";
+              return (
                 <div className="mr-5 ">
                   <div className="w-60 h-72  hover:shadow-xl hover:cursor-pointer hover:scale-105 transform transition duration-300">
-                    <div className= "w-full h-52 flex items-center justify-center rounded-t-lg" style={{ backgroundColor: product.bgcolor || 'blue' }}>
+                    <div
+                      className="w-full h-52 flex items-center justify-center rounded-t-lg"
+                      style={{ backgroundColor: product.bgcolor || "blue" }}
+                    >
                       <img
                         className="h-[12rem] drop-shadow-[0_10px_15px_rgba(0,0,0,0.4)]"
                         // src={`data:image/png;base64,${base64String}`}
@@ -83,10 +105,23 @@ const Home = () => {
                         alt="Product Image"
                       />
                     </div>
-                    <div className="flex justify-between bg-gray-700 bg-[<%= product.panelcolor %>] items-center px-4 py-4 text-white rounded-b-lg" style={{ backgroundColor: product.panelcolor || 'blue' }}>
+                    <div
+                      className="flex justify-between bg-gray-700 bg-[<%= product.panelcolor %>] items-center px-4 py-4 text-white rounded-b-lg"
+                      style={{ backgroundColor: product.panelcolor || "blue" }}
+                    >
                       <div>
-                        <h3 style={{ color: product.textcolor || 'blue' }} className="text-lg font-semibold">{product.name}</h3>
-                        <h4 className="text-lg font-semibold" style={{ color: product.textcolor || 'blue' }}>₹ {product.price}</h4>
+                        <h3
+                          style={{ color: product.textcolor || "blue" }}
+                          className="text-lg font-semibold"
+                        >
+                          {product.name}
+                        </h3>
+                        <h4
+                          className="text-lg font-semibold"
+                          style={{ color: product.textcolor || "blue" }}
+                        >
+                          ₹ {product.price}
+                        </h4>
                       </div>
                       <h3
                         className="w-7 h-7 flex items-center justify-center rounded-full bg-white"
@@ -97,8 +132,23 @@ const Home = () => {
                     </div>
                   </div>
                 </div>
-              )
-                } )}
+              );
+            })}
+            <div className="justify-center mx-auto flex gap-10 p-5">
+              {totalPages?.map((page, index) => (
+                <button
+                  className={`px-4 py-2 border rounded ${
+                    activePage === page
+                      ? "transform scale-125 bg-blue-500 text-white"
+                      : "bg-gray-300"
+                  } transition-transform duration-100`}
+                  key={index}
+                  onClick={() => handlepageclick(index)}
+                >
+                  {page}
+                </button>
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -107,5 +157,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
